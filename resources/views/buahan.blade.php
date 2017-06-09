dfc5f8fccabcd14b1a656d23f6c2ca21f1373e64
151610222-smkassalaambandung <151610222@smkassalaambandung.sch.id>
2017-06-08 23:56:21 -0700

    belajar controller

---
+++ resources/views/buahan.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	          <br>
+		@foreach($id as $data)
+			{{$data}}<hr>
+			@endforeach
+
+</body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,45 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/produk', function () {
+	$a = App\produk::all();
+	$w = App\pengguna::all();
+	$c = App\pengaturan::all();
+    return $a .'<br><br>' .$w .'<br><br>'. $c;
+});
+
+Route::get('/pengguna', function () {
+	$b = App\pengguna::all();
+    return $b;
+});
+
+Route::get('/pengaturan', function () {
+	$c = App\pengaturan::all();
+    return $c;
+});
+
+Route::get('/test','MyController@percobaan');
+Route::get('/test2','MyController@percobaan2');
+Route::get('/test3','MyController@percobaan3');
+Route::get('/test4','MyController@percobaan4');
+Route::get('/test5','MyController@percobaan5');
+Route::get('/test6','MyController@percobaan6');
+Route::get('/test7','MyController@percobaan7');
+
+Route::get('/test8/{data}','MyController@buah');
+   
+
